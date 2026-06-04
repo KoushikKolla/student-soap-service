a69475558be6808bb77ed62d59f0035e5b1cbec9
KoushikKolla(Ext) <koushikkolla51@gmail.com>
2026-06-04 14:27:55 +0530

    Added header with clientId and requestId

--- src/main/java/com/example/student_soap_service/config/SoapConfig.java
+++ src/main/java/com/example/student_soap_service/config/SoapConfig.java
@@ -1,19 +1,24 @@
 package com.example.student_soap_service.config;
 
+import com.example.student_soap_service.interceptor.ClientHeaderInterceptor;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurer;
+import org.springframework.ws.server.EndpointInterceptor;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 
+import java.util.List;
+
 @EnableWs
 @Configuration
-public class SoapConfig {
+public class SoapConfig implements WsConfigurer {
 
     @Bean
     public ServletRegistrationBean<MessageDispatcherServlet>
@@ -58,5 +63,12 @@ public class SoapConfig {
         wsdl.setSchema(studentSchema);
 
         return wsdl;
+    }
+    @Override
+    public void addInterceptors(
+            List<EndpointInterceptor> interceptors) {
+
+        interceptors.add(
+                new ClientHeaderInterceptor());
     }
 }
